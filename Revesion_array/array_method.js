956328e25efc6521c8dddffc448562373ee03989
vishal1202verma <vishalvermao.pcm121@gmail.com>
2023-04-30 01:08:12 +0530

    second commit

---
+++ Revesion_array/array_method.js
@@ -0,0 +1,20 @@
+// for removing element from array 
+let arr = [1, 2 ,3, 4 , 5, 6 , 7]
+console.log(arr.pop());
+console.log(arr);
+
+// index finding o f aray 
+console.log(arr.indexOf(5));
+console.log(arr.at(5)); // at will tellthe value of index 
+//  shift and unshift 
+
+console.log(arr.shift());// shift by left side 
+console.log(arr);
+
+console.log(arr.unshift()) // shidt by right side 
+console.log(arr);
+
+
+
+
+
